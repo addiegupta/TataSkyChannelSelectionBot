d92288c629e1fa53204f37291e0233090e3dc531
Aditya <addiegupta@gmail.com>
2019-02-05 13:55:13 +0530

    add channels list, update readme, finalise changes

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# TataSkyChannelSelectionScript
-Takes the selected channels from an xlsx sheet and selects them on Tata Sky Channel Selection Portal
+# Tata Sky Channel Selection Bot
+
+This Python script selects the channels from a Spreadsheet (.xlsx) and selects them on Tata Sky's new channel selection portal.
+
+##Usage
+
+-Clone the repository/ Download ZIP
+
+-In the *channels.xlsx" file, remove the *#* from the channels that you want and save it
+
+OR
+
+Create an xlsx file having required channel names in column B.
+
+-Open a terminal in the files location and run
+
+`python select_channels.py "<registered-mobile-number>" "<xlsx-file-name>"`
+
+- Enter OTP when required
+
+- Wait for the channels to get selected
+
+- Finalise changes
+
+
+<table>
+        <tr>
+<td><img src = "https://user-images.githubusercontent.com/22665789/52260588-983e8c00-294c-11e9-94cd-bb2467d5f511.gif" height = "322" width="600"></td>
+    </tr>
+</table>
+
+
+##Libraries Used
+
+[Selenium: Browser Automation](https://www.seleniumhq.org/)
+
+[Pandas: Python Data Analysis Library](https://pandas.pydata.org/)
--- select_channels.py
+++ select_channels.py
@@ -8,41 +8,46 @@ from selenium.webdriver.common.keys import Keys
 from optparse import OptionParser
 
 
+# Gets channel list from excel sheet
 def parse_excel_sheet(file_loc):
-    df = pd.read_excel(file_loc, index_col=None, na_values=['NA'], usecols="A")
+    df = pd.read_excel(file_loc, index_col=None, na_values=['NA'], usecols="B")
     channels = []
     for index, row in df.iterrows():
-        if type(row[0]) is not float:
+        if (type(row[0]) is not float) and (row[0][0] != '#'):
             channels.append(row[0])
 
     return channels
 
-
+# Selenium code to select channels
 def start_selecting_channels(mobile_number, file_name):
 
+    # Load webpage
     driver = webdriver.Chrome("Drivers\chromedriver.exe")
     driver.set_page_load_timeout(10)
-
-    print("10 second timeout")
-
     link = "https://packselection.tatasky.com/PRRedirect/PRWebMQ"
     driver.get(link)
 
+    print("10 second timeout for loading site")
+
+    # Enter mobile number and wait for OTP to be entered
     driver.find_element_by_name("rmn").send_keys(mobile_number)
     driver.find_element_by_css_selector("button[type='submit']").send_keys(Keys.ENTER)
     time.sleep(2)
     driver.find_element_by_xpath("//*[@id='placeHold']").click()
-    print("20 seconds to enter OTP")
+    print("Enter OTP within 20 seconds")
     time.sleep(20)
 
+    #Get channels from excel sheet
     channels = parse_excel_sheet(file_name)
 
+    # Open channel selection page
     driver.find_element_by_xpath("/html/body/section/div/div/div/div[3]/div/h5/a/span[1]").click()
     time.sleep(5)
     driver.find_element_by_xpath("//a[@href='#collapse1']").click()
     time.sleep(2)
     driver.find_element_by_xpath("//a[@href='#collapse3']").click()
 
+    # Select channels
     for channel in channels:
         # Search channel
         driver.find_element_by_xpath("//*[@id='imaginary_container']/div/input").send_keys(channel)
@@ -58,6 +63,7 @@ def start_selecting_channels(mobile_number, file_name):
         # Clear searchbox
         driver.find_element_by_xpath("//*[@id='imaginary_container']/div/input").clear()
 
+    # Open selected channels list
     driver.find_element_by_xpath("/html/body/section/div[1]/div/p[1]/button").click()
     time.sleep(4)
 
